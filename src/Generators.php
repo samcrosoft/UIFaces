e40460b163ff706c9ddcd23a26395329da11b819
samuel Olowofela <samuel_olowofela@yahoo.com>
2015-07-24 21:47:02 +0100

    updated the code to reflect guzzle 6

--- src/Generators.php
+++ src/Generators.php
@@ -2,7 +2,9 @@
 namespace Samcrosoft\UIFaces;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\RequestException;
-use GuzzleHttp\Pool;
+use GuzzleHttp\Promise;
+use GuzzleHttp\Psr7\Response;
+use Psr\Http\Message\ResponseInterface;
 use Samcrosoft\UIFaces\Transformers\UIFace;
 
 
@@ -18,7 +20,7 @@ class Generators
 
 
 	/**
-	 *
+	 * Client
 	 */
 	private $oGuzzleClient = null;
 
@@ -41,11 +43,11 @@ class Generators
 	}
 
 	/**
-	 * 
+	 * @param string $sRequest
+	 * @param $sExtra
+	 * @return mixed|\Psr\Http\Message\ResponseInterface
 	 */
-	private function createFacesRequest($sRequest, $sExtra){
-		$sRandomUserLink =  "http://uifaces.com/api/v1/random";
-
+	private function createFacesRequest($sRequest = self::RANDOM_USER_REQUEST, $sExtra=null){
 		switch ($sRequest) {
 			case self::RANDOM_USER_REQUEST:
 				$sRandomUserLink =  "http://uifaces.com/api/v1/random";
@@ -56,12 +58,16 @@ class Generators
 				break;
 		}
 
-		return $this->getClientObject()->createRequest('GET', $sRandomUserLink);
+		return $this->getClientObject()->get($sRandomUserLink);
 	}
 
 
 	/**
 	 * This will make the request to the uifaces api server
+	 * @param string $sRequest
+	 * @param null $sExtra
+	 * @return array|\Psr\Http\Message\StreamInterface
+	 * @throws \Exception
 	 */
 	protected function makeRequestToUiFaces($sRequest = self::RANDOM_USER_REQUEST, $sExtra = null){
 		$oGuzzleClient = $this->getClientObject();
@@ -69,10 +75,9 @@ class Generators
 		if(is_null($oGuzzleClient) || !( $oGuzzleClient instanceof Client))
 			throw new \Exception("HTTP Client Not Initiated");
 
-		$aReturn = [];
 		try{
-			$oResponse = $this->getClientObject()->send($this->createFacesRequest($sRequest, $sExtra));	
-			$aReturn = $oResponse->json();
+			$oResponse = $this->getClientObject()->send($this->createFacesRequest($sRequest, $sExtra));
+			$aReturn = json_decode($oResponse->getBody());
 		}catch (RequestException $e){
 			$aReturn =[];
 		}
@@ -101,6 +106,7 @@ class Generators
 
 	/**
 	 * @param string $sUsername
+	 * @return UIFace
 	 */
 	public function getUser($sUsername){
 		$oObject = $this->makeRequestToUiFaces(self::SPECIFIC_USER_REQUEST, $sUsername);
@@ -110,6 +116,7 @@ class Generators
 	/**
 	 * @param int $iCount
 	 * @return array
+	 * @deprecated -> resolve this
 	 */
 	public function getBatchUsers($iCount = 10){
 		$aRequests = [];
@@ -119,12 +126,16 @@ class Generators
 		}
 
 		// send the batch
-		$oResults = Pool::batch($this->getClientObject(), $aRequests);
+		//$oResults = Pool::batch($this->getClientObject(), $aRequests);
 
+		$oResults = Promise\unwrap($aRequests);
 		// get all the successful ones
 		$aResults = [];
-		foreach ($oResults->getSuccessful() as $response) {
-			$aResults[] = $this->transformResponseToTransformer($response->json());
+		foreach ($oResults as $response) {
+			/** @var ResponseInterface $response */
+			/** @var array $sBody */
+			$sBody = json_decode($response->getBody());
+			$aResults[] = $this->transformResponseToTransformer($sBody);
 		}
 
 		return $aResults;
